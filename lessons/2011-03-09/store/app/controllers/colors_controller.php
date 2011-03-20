9955347651e5e99a78beda1c919c49012836399d
unknown <the0ther@.(none)>
2011-03-20 03:14:23 -0400

    this change makes the json response work

--- lessons/2011-03-09/store/app/controllers/colors_controller.php
+++ lessons/2011-03-09/store/app/controllers/colors_controller.php
@@ -1,13 +1,20 @@
 ﻿<?
 class ColorsController extends AppController {
 	var $components = array('RequestHandler');
-
+	
 	function colors_for_style($style=null) {
 		$this->layout = 'ajax';
 		if ( $this->RequestHandler->isAjax() ) {
 			Configure::write('debug',0);
 			$this->RequestHandler->setContent('JSON');
-			$colors = null; //TODO: fix this...
+			$this->Color->contain(array(
+				'Boot' => array(
+					'conditions' => array('Boot.id'=>$style)
+				)
+			));
+			$colors = $this->Color->find('all');
+			$colors = Set::extract('/Boot/id/../../Color',$colors);
+			$this->log(print_r($colors,true));
 			$this->set('colors',json_encode($colors)); 
 		} else {
 			header();
